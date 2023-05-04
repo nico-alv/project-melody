a4d57139c9db6f3d2e88af38e287dd2a8eac3d19
Andrea Saldaña Cavour <andrea.saldana@alumnos.ucn.cl>
2023-05-04 18:07:16 -0400

    refactor: vista principal

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('concert.index');
+    return view('layout.app');
 })->name('welcome');
 
 // Rutas de registro
