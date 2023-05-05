184f61c9c81cc12f366454c20d47da417d875bb2
Nicolás Álvarez <130405041+nico-alv@users.noreply.github.com>
2023-05-05 01:53:58 -0400

    refactor: nombre del controlador
    Se sigue el estándar UpperCamelCase de acuerdo al estándar de programación.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Sessioncontroller extends Controller
+class SessionController extends Controller
 {
     public function create(){
         return view ('auth.login');
