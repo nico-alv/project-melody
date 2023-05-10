0afa3895d3a6865f62318e1c7e0a1fa36f63fb33
luciano90a <114699007+luciano90a@users.noreply.github.com>
2023-05-09 23:12:47 -0400

    Update login.blade.php
    login con estandar establecido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,35 @@
 @extends('layout.app')
-
 @section('title')
-
+Iniciar Sesión
+@endsection
 @section('content')
-
-    <div class=" flex justify-center">
-        <form class="mt-4 bg-gray-200  rounded-lg shadow-lg  w-3/6 " method="POST" action=" {{route('login')}} " novalidate >
-            <h1 class=" text-center uppercase font-bold text-3xl p-4 "> Inicia sesión  </h1>
+<div class="flex md:flex-row md:justify-center ">
+    <div class="md:w-2/3 bg-gradient-to-r from-orange-light to-orange-dark  p-6 rounded-xl shadow-lg  ">
+        <form action="{{route('login')}}" method="POST" novalidate>
+            <div class = "">
+                <h2 class="bg-orange-medium-light text-center  uppercase font-bold text-3xl p-4 rounded-xl">Iniciar Sesión</h2>
+            </div>
             @csrf
             @if (session('message'))
-            <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ session('message') }}</p>
+                <p>{{ session('message') }}</p>
             @endif
-            <div class="flex justify-center">
+            <div class="mb-6">
+                <label for="email" class="text-black-dark font-bold"> Correo electrónico </label>
+                <input id="email" name='email' placeholder="Ingrese su correo electrónico" class="shadow rounded-lg w-full mt-1 py-2 px-3"/>
                 @error('email')
-                <p class="border border-red-500 rounded-md bg-red-100
-                text-red-600 p-2 my-2 w-3/4 text-center">{{$message}}</p>
+                    <p class="bg-pink-medium-light border border-pink-dark text-pink-dark mt-1 px-4 py-2 rounded-full">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="flex justify-center">
-                <input type="email" class="w-2/3 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Email"
-                id="email" name="email">
-            </div>
-
-            <div class="flex justify-center">
+            <div class="mb-6">
+                <label for="password" class="text-black-dark font-bold">Contraseña</label>
+                <input id="password" name="password" type="password" placeholder="Ingrese su contraseña" class="shadow rounded-lg w-full mt-1 py-2 px-3"/>
                 @error('password')
-                <p class=" mt-6 border border-red-500 rounded-md bg-red-100
-                text-red-600 p-2 my-2 text-center w-3/4">{{$message}}</p>
+                    <p class="bg-pink-medium-light border border-pink-dark text-pink-dark mt-1 px-4 py-2 rounded-full">{{ $message }}</p>
                 @enderror
             </div>
-            <div class=" flex justify-center">
-            <input type="password" class=" mt-2 w-2/3 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light " placeholder="Password"
-            id="password" name="password">
+            <div class="flex justify-center bg-yellow-medium-light hover:bg-yellow-light rounded-lg">
+                <button type="submit" class="text-black font-medium rounded-lg text-sm p-3 text-center w-full">Iniciar Sesión</button>
             </div>
-             <div class="flex justify-center">
-                <button type="submit" class=" flex justify-center rounded-md bg-blue-700  text-lg
-                text-white font-semibold p-2 my-3 w-1/2 hover:bg-blue-800"> Confirmar </button>
-             </div>
         </form>
+    </div>
 @endsection
