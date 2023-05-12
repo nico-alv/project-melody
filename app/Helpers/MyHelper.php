a19f1563b82d9b9bf5cfff2ae0493581ffa005e7
luciano90a <luciano_90lsv@hotmail.com>
2023-05-12 18:03:28 -0400

    refactor: adicion de mensaje price.numeric

--- app/Helpers/MyHelper.php
+++ app/Helpers/MyHelper.php
@@ -21,6 +21,7 @@ function makeMessages()
         'email.unique' => 'El correo ingresado ya esta existe en el sistema, Intente Iniciar sesión',
         'password.min' => 'La contraseña posee menos de 8 carácteres',
         'price.min' => 'El valor de la entrada no puede ser inferior a $20.000 pesos.',
+        'price.numeric' => 'El valor de la entrada debe ser numerico',
         'name.regex' => 'El nombre contiene carácteres no permitidos, Ingrese solo letras',
         'password.regex' => 'La contraseña ingresada no es alfanumérica',
     ];
