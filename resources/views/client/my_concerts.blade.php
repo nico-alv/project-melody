740c1fc0e8887a757c0745b5d47e74f03ac79e5f
luciano90a <luciano_90lsv@hotmail.com>
2023-06-13 14:08:52 -0400

    fix:colores detalles de compra

--- resources/views/client/my_concerts.blade.php
+++ resources/views/client/my_concerts.blade.php
@@ -7,8 +7,8 @@
 
 @section('content')
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table class="w-full text-sm text-left text-black-dark">
+            <thead class="text-xs text-black-dark uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3 bg-orange-light">
                         Numero reserva
