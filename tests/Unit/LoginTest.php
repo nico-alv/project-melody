ee2db1f855deddd26c7dcf9311b93a768a27b648
luciano90a <luciano_90lsv@hotmail.com>
2023-06-18 20:51:26 -0400

    feat: pruebas unitarias

--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -34,4 +34,22 @@ class LoginTest extends TestCase
         $response->assertRedirect('dashboard'); // Verifica que se redirija a la página de dashboard
         $this->assertAuthenticatedAs($user); // Verifica que el usuario esté autenticado
     }
+
+    public function testfailSuccessLogin()
+{
+    $user = User::create([
+        'name' => 'usertest1',
+        'email' => 'usertest1@correo.com',
+        'role' => 1,
+        'password' => bcrypt('1234567k')
+    ]);
+
+    $response = $this->post('/login', [
+        'email' => 'usertest1@correo.com',
+        'password' => '1234567p',
+    ]);
+
+    $response->assertStatus(302); // Verifica el código de estado de la respuesta (redirección)
+    $this->assertGuest(); // Verifica que el usuario no esté autenticado
+}
 }
