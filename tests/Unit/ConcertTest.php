2a7338c2c773dd5ee40e1cf696a255e64a4e26d8
nico-alv <nicolas.alvarez02@alumnos.ucn.cl>
2023-06-19 21:54:37 -0400

    feat: pruebas
    Co-authored-by: Andrea Saldaña <coralinegrl@users.noreply.github.com>

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -12,34 +12,61 @@ class ConcertTest extends TestCase
     protected function tearDown(): void
     {
         $user = User::where('name', 'admin de prueba')->first();
-
+        $concert = Concert::where('concert_name', 'Concierto de prueba')->first();
         if ($user) {
             $user->delete();
         }
+        if ($concert) {
+            $concert->delete();
+        }
         parent::tearDown();
     }
-    
-    public function testInvalidConcertPrice()
+
+
+    public function testSuccessfulConcertCreation()
     {
         $admin = User::create([
             'name' => 'admin de prueba',
-            'email' => 'admin@correo.com',
+            'email' => 'testadmin@correo.com',
             'role' => 'Administrador',
             'password' => bcrypt('1234567k')
         ]);
         $response = $this->post('/login', [
-            'email' => 'admin@correo.com',
+            'email' => 'testadmin@correo.com',
             'password' => '1234567k',
         ]);
-        $response->assertStatus(302);
         $this->assertAuthenticatedAs($admin);
         $concertData = [
             'concert_name' => 'Concierto de prueba',
-            'price' => 1,
+            'price' => 1000000,
             'stock' => 100,
             'date' => Carbon::today()->addDays(rand(3, 30))->format('Y-m-d')
         ];
         $response = $this->post('/concert', $concertData);
-        $response->assertSessionHasError('date');
+        $response->assertSessionDoesntHaveErrors([
+            'concert_name',
+            'price',
+            'stock',
+            'date'
+        ]);
+        $response->assertRedirect('/dashboard');
+    }
+
+
+    public function testInvalidConcertPrice()
+    {
+        $response = $this->post('/login', [
+            'email' => 'italo.donoso@ucn.cl',
+            'password' => 'Melody91',
+        ]);
+        $response->assertStatus(302);
+        $concert = [
+            'concert_name' => 'prueba',
+            'price' => 1,
+            'stock' => 100,
+            'date' => Carbon::today()->addDays(5)->format('Y-m-d')
+        ];
+        $response = $this->post('/concert', $concert);
+        $response->assertSessionHasErrors('price');
     }
 }
--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -34,4 +34,21 @@ class LoginTest extends TestCase
         $response->assertRedirect('dashboard'); // Verifica que se redirija a la página de dashboard
         $this->assertAuthenticatedAs($user); // Verifica que el usuario esté autenticado
     }
+    public function testAdminAccess()
+    {
+        $user = User::create([
+            'name' => 'administrador',
+            'email' => 'administrador@correo.com',
+            'role' => 'Administrador',
+            'password' => bcrypt('1234567k')
+        ]);
+        $response = $this->post('/login', [
+            'email' => 'usertest@correo.com',
+            'password' => '1234567k',
+        ]);
+        $concerts = Concert::where('date', '>', $currentDate)->orderBy('date', 'asc')->get();
+        $response->assertStatus(302);
+        $response->assertRedirect('dashboard');
+        $this->assertAuthenticatedAs($user);
+    }
 }
