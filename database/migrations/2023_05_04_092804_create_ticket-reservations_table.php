74d0c947fb5899e8ea1dbf0881ce1cd477ec26b0
NecroHunterr <exequiel.gonzales@gmail.com>
2023-06-25 01:03:57 -0400

    fix: ortografia

--- database/migrations/2023_05_04_092804_create_ticket-reservations_table.php
+++ database/migrations/2023_05_04_092804_create_ticket-reservations_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('ticket_quantity');
             $table->string('payment_method');
             $table->integer('total');
-            $table->dateTime('purchase_date');
+            $table->datetime('purchase_date');
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('concert_id')->constrained('concerts');
         });
